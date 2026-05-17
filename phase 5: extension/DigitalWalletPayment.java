90ea4d31f61babda516d6ae32bf1a5ddc9952313
AnfalFaruqi <anfalfrqi5647@gmail.com>
2026-05-17 14:54:36 +0500

    Add DigitalWalletPayment class for digital payments

---
+++ phase 5: extension/DigitalWalletPayment.java
@@ -0,0 +1,10 @@
+package supermarket;
+
+// new feature: digital payment
+public class DigitalWalletPayment implements PaymentMethod {
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("\t\tProcessing Digital Wallet payment of $" + amount);
+        System.out.println("\t\tPayment successful via EasyPaisa/JazzCash.");
+    }
