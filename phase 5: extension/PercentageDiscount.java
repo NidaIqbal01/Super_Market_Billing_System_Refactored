11d67aa1d3b76bdb04344f63183fe1704fefa0ec
AnfalFaruqi <anfalfrqi5647@gmail.com>
2026-05-17 14:53:40 +0500

    Add PercentageDiscount class for discount strategy

---
+++ phase 5: extension/PercentageDiscount.java
@@ -0,0 +1,16 @@
+package supermarket;
+
+// OCP Extension: Percentage based discount strategy
+public class PercentageDiscount implements DiscountStrategy {
+
+    private final double percentage;
+
+    public PercentageDiscount(double percentage) {
+        this.percentage = percentage;
+    }
+
+    @Override
+    public double applyDiscount(double subtotal) {
+        return subtotal * (percentage / 100);
+    }
+}
